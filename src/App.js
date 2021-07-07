a070a28a2c64c4262c17251d64c55b26eed2c1c5
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-07-07 09:19:39 -0500

    no message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Main from "./components/MainComponent";
+import Dashboard from "./components/MainComponent";
 
 class App extends Component {
   render() {
--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -14,6 +14,7 @@ function DashboardPage() {
               <div className="col-8">
                 <NewPostModal />
               </div>
+              <div className="col-8"></div>
             </div>
           </div>
         </div>
